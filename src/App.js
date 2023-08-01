b62e35234fb2624eae9dae828b178d396dbc9deb
tapasf24 <tapasranjanbehera7205@gmail.com>
2023-08-01 20:29:05 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navigation from './component/Navigation';
+import HeroSection from './component/HeroSection';
+import './App.css'; 
+
+function App() {
+  return (
+    <div>
+    <Navigation/>
+    <HeroSection/>
+    </div>
+  );
+}
+
+export default App;
